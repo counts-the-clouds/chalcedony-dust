65daed70b8525444fc9f2ed769a19e62d9219c0c
Counts The Clouds <counts-the-clouds@water-through-fog.com>
2024-07-17 18:13:35 -0500

    Remove example

--- src/experimental/pixi/pixi-boot.js
+++ src/experimental/pixi/pixi-boot.js
@@ -6,29 +6,8 @@ PlaygroundRegistry.register('pixi',{ init, run,
 function init() {}
 
 async function run() {
-  const width = window.innerWidth;
-  const height = window.innerHeight;
+  const app = new PIXI.Application();
+  await app.init({ background:'#131320', resizeTo:window });
 
-  const application = new PIXI.Application();
-  await application.init({ width, height });
-
-  X.first('#pixiCanvas').appendChild(application.canvas);
-
-  await PIXI.Assets.load(`${APP}/assets/icon.png`);
-  let sprite = PIXI.Sprite.from(`${APP}/assets/icon.png`);
-
-  application.stage.addChild(sprite);
-
-  let elapsed = 0.0;
-  application.ticker.add((ticker) => {
-
-    // DeltaTime is "scalar" time, which I think depends on the FPS or something?
-    // elapsed += ticker.deltaTime;
-
-    // elapsedMS is actual time
-    elapsed += ticker.elapsedMS;
-    // console.log(Math.floor(elapsed/1000),' > ',ticker.elapsedMS);
-
-  });
-
+  X.first('#pixiCanvas').appendChild(app.canvas);
 }
