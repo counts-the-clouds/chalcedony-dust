54dfb2a51ed95e5d5e2cf79c5521d42e11f83909
Counts The Clouds <counts-the-clouds@water-through-fog.com>
2024-08-07 11:34:59 -0500

    Adding shadow

--- data/shapes/core.js
+++ data/shapes/core.js
@@ -2,6 +2,12 @@ ShapeRegistry.register('core', {
 
   drawBase: graphics => {
     graphics.regularPoly(_tileSize/2, _tileSize/2, 56, 8, 0.3926991);
+
+    const shadow = new Pixi.Sprite(AssetLibrary.fetch('cell-inner-highlight-1'));
+    shadow.x = 50
+    shadow.y = 50
+
+    graphics.addChild(shadow)
   },
 
 
