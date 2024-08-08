2f2ece72911ef57508df72f9d1ec6965ea1890f7
Counts The Clouds <counts-the-clouds@water-through-fog.com>
2024-08-07 19:47:55 -0500

    Add corner room

--- data/gameStages/baseline.js
+++ data/gameStages/baseline.js
@@ -4,6 +4,26 @@ GameStageRegistry.register(_baseline,{
 
   placedTiles:[
     { x:0, y:0, code:'dungeon-core' },
+
+    { x:1, y:1, code:'baseline-h1-0' },
+    { x:2, y:2, code:'baseline-h1-1' },
+    { x:3, y:3, code:'baseline-h1-2' },
+
+    { x:3, y:1, code:'baseline-r1-0' },
+    { x:4, y:2, code:'baseline-r1-1' },
+    { x:5, y:3, code:'baseline-r1-2' },
+
+    { x:5, y:1, code:'baseline-h2-0' },
+    { x:6, y:2, code:'baseline-h2-1' },
+    { x:7, y:3, code:'baseline-h2-2' },
+
+    { x:7, y:1, code:'baseline-h1-r1-0' },
+    { x:8, y:2, code:'baseline-h1-r1-1' },
+    { x:9, y:3, code:'baseline-h1-r1-2' },
+
+    { x:1, y:4, code:'baseline-r2-0' },
+
+
   ],
 
 });
