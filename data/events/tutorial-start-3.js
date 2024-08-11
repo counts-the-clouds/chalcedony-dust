6244666b66d429b681907433ff83c505732a862c
Counts The Clouds <counts-the-clouds@water-through-fog.com>
2024-08-10 19:18:36 -0500

    Update event

--- data/events/tutorial-start-3.js
+++ data/events/tutorial-start-3.js
@@ -1,16 +1,25 @@
 EventRegistry.register('tutorial-start-3', {
 
   stages:[{
+    showWhenEqual:['type',_hall],
     pages: [
-      { text:`Something has been completed... but I should actually figure out was just completed first.` },
+      { text:`The rough stone walls of the corridor seem to straighten and expand as it opens into the newly formed
+              space. The hall that was little more than a roughly carved passageway though the stone now looks more
+              complete, though still empty, waiting to be filled with deadly traps.` },
     ]
+  },{
+    showWhenEqual:['type',_room],
+    pages: [
+      { text:`The rough stone walls of the room seem to straighten and expand as it opens into the newly formed
+              space. The room that was little more than a roughly hollowed out cavern cave now looks more complete,
+              though still empty, waiting to be filled with minions and the machinery of a working dungeon.` },
+
+    ],
   }],
 
   onBefore: context => {
     FeatureDataStore.all().forEach(feature => {
-      if (feature.isComplete()) {
-        context.featureType = feature.getType();
-      }
+      if (feature.isComplete()) { context.type = feature.getType(); }
     });
 
     return context;
