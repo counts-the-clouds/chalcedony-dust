abbf005301b0a9eecc828ca18c1fd671f9724471
Counts The Clouds <counts-the-clouds@water-through-fog.com>
2024-08-21 13:24:20 -0500

    Use wedges here

--- data/tiles/baseline-r2-n1-0.js
+++ data/tiles/baseline-r2-n1-0.js
@@ -1,8 +1,8 @@
 TileRegistry.register('baseline-r2-n1-0', {
 
   segments:[
-    { type:TileType.room, exits:[_e], graphics:{ shape:'room-nubbin', rotate:1 }},
-    { type:TileType.room, exits:[_w], graphics:{ shape:'room-nubbin', rotate:3 }},
+    { type:TileType.room, exits:[_e], graphics:{ shape:'room-wedge', rotate:1 }},
+    { type:TileType.room, exits:[_w], graphics:{ shape:'room-wedge', rotate:3 }},
     { type:TileType.node, exits:[], graphics:{ shape:'node-large' }},
   ],
 
