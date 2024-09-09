9441b19db1f370c34a96da79f1db0e508c34c692
Counts The Clouds <counts-the-clouds@water-through-fog.com>
2024-09-09 09:53:47 -0500

    Update worker select

--- src/components/features/worker-control.js
+++ src/components/features/worker-control.js
@@ -4,16 +4,18 @@ global.WorkerControl = (function() {
     const construction = feature.getConstruction();
 
     const list = X.createElement(`<ul class='worker-list'></ul>`);
-    const select = X.createElement(`<div class='worker-select hide'></div>`);
+    const workerSelect = X.createElement(`<div class='worker-select hide'></div>`);
     buildSlots(list, construction);
 
     const control = X.createElement(`<div class='worker-control'></div>`);
     control.appendChild(list);
-    control.appendChild(select);
+    control.appendChild(workerSelect);
+
+    workerSelect.addEventListener('mouseleave', () => { closeMinionList(control); })
 
     list.querySelectorAll('li').forEach(item => {
       item.addEventListener('click', event => {
-        openMinionList(feature, control);
+        openMinionList(feature, control, event.target.getAttribute('data-slot'));
       });
     });
 
@@ -24,32 +26,64 @@ global.WorkerControl = (function() {
     const configuration = construction.getWorkerConfiguration();
     const workers = construction.getWorkers();
 
+    // TODO: When we build list list we should display the skill (or any other
+    //       appropriate bonus that the minion has for this job. Currently we
+    //       don't really have skills or skills associated with job, but we
+    //       will at some point.
+    const skill = 'mining'
+
     list.innerHTML = '';
 
     for (let i=0; i<configuration.slots; i++) {
-      const slot = X.createElement(`<li class='slot slot-${i}'><div class='remove'></div><div class='display'></div></li>`);
-      const display = slot.querySelector('.display');
-
-      if (workers[i]) {
-        display.innerHTML = `<div class='name'>${workers[i].getFullName()}</div>`;
-      } else {
-        display.innerText = `Select Minion`;
-        X.addClass(display,'empty');
-        X.addClass(slot.querySelector('.remove'), 'hide');
-      }
-
-      list.appendChild(slot);
+      list.appendChild(workers[i] ?
+        buildMinionItem(workers[i], skill, i) :
+        X.createElement(`<li class='minion empty' data-slot='${i}'>Select Minion</li>`));
     }
   }
 
-  function openMinionList(feature, control) {
-    const select = control.querySelector('.worker-select');
-    select.innerHTML = '';
+  // TODO: Get actual skill bonus from minion.
+  function buildMinionItem(minion, skill, i) {
+    const element = X.createElement(`<li class='minion'>
+      <div class='remove'></div>
+      <div class='species'>${minion.getSpecies().getName()}</div>
+      <div class='name'>${minion.getFullName()}</div>
+      <div class='skill'>${skill} 0</div>
+    </li>`);
+
+    if (i != null) {
+      element.setAttribute(`data-slot`,`${i}`);
+    }
+
+    return element;
+  }
+
+  function openMinionList(feature, control, slot) {
+    const position = X.getPosition(control);
+    const workerList = X.createElement(`<ul class='select-list'></ul>`);
+    const workerSelect = control.querySelector('.worker-select');
+    workerSelect.appendChild(workerList);
+    workerSelect.style.top = `${position.top}px`;
+    workerSelect.style.left = `${position.left + 10}px`;
+
+    X.removeClass(workerSelect,'hide');
 
     MinionHelper.allUnassignedMinions().forEach(id => {
       const minion = MinionDataStore.get(id);
-      console.log(`Unassigned: ${minion}`);
+      const item = buildMinionItem(minion,'mining');
+      item.addEventListener('click', () => { selectMinion(slot, control, minion, feature); });
+      workerList.appendChild(item);
     });
+  }
+
+  function closeMinionList(control) {
+    const workerSelect = control.querySelector('.worker-select');
+    workerSelect.innerHTML = '';
+    X.addClass(workerSelect,'hide');
+  }
+
+  function selectMinion(slot, control, minion, feature) {
+    closeMinionList(control);
+    console.log(`Selected - Slot:${slot} ${minion} ${feature}`);
   }
 
   return Object.freeze({ build })
